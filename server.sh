c5812553462a74ad15e518538b5111720926b4e7
Nikita Potapov <47923521+nikita-potapov@users.noreply.github.com>
2023-06-25 13:54:57 +0400

    update requirements.txt

--- server.sh
+++ server.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-if [ ! -d "cicdserver_venv" ]
+if [ ! -d "venv" ]
 then
-  '/usr/bin/python3 -m venv cicdserver_venv'
+  python3 -m venv venv
 fi
 
-'bash cicdserver_venv/activate'
-'pip3 install -r requirements.txt'
+bash cicdserver_venv/activate
+pip3 install -r requirements.txt
 
-'uvicorn main:app'
+uvicorn main:app
